a7aeace03897f55f7d4a82d5e8e46bc93c69f781
Egor Berdnikov <egorr.berd@gmail.com>
2012-11-04 00:18:56 +0300

    Add initial configuration for emacs-24.

---
+++ emacs-24-new/init.el
@@ -0,0 +1,18 @@
+;; Set marmalade repository
+(require 'package)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+;; Check that package archive is loaded
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Load this package if they not already loaded
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings
+                                  starter-kit-eshell starter-kit-js starter-kit-ruby)
+  "A list of packages to ensure are installed at launch")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
