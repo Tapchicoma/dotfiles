bf0d508a4e06bc3859d02b73ca7ef3720506e5e9
Yahor Berdnikau <egorr.berd@gmail.com>
2017-02-02 00:00:43 +0100

    Add settings personal font face and theme.

---
+++ emacs-prelude/personal/visual.el
@@ -0,0 +1,10 @@
+;; Visual customizations
+
+(add-to-list 'default-frame-alist '(font . "-bitstream-Hack-normal-normal-normal-*-24-*-*-*-m-0-iso10646-1"))
+(set-face-attribute 'default t :font "-bitstream-Hack-normal-normal-normal-*-24-*-*-*-m-0-iso10646-1")
+
+(set-face-attribute 'default nil :font "-bitstream-Hack-normal-normal-normal-*-24-*-*-*-m-0-iso10646-1")
+(set-frame-font "-bitstream-Hack-normal-normal-normal-*-24-*-*-*-m-0-iso10646-1" nil t)
+
+;; Setting theme
+(load-theme 'misterioso t)
