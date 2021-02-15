0dc18ca52d5838b9ad6f55a5931aa24a4e3b1a9e
Yahor Berdnikau <egorr.berd@gmail.com>
2021-02-15 09:49:34 +0100

    Move all installation steps into separate functions.

--- install.sh
+++ install.sh
@@ -1,48 +1,12 @@
 #!/bin/bash
 
-# Installs all symlinks for current user
+### Common variables
+
+# Path to the dotfiles repository
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Private configurations directory
 SHARED_CONFIG_DIR=$HOME/cloud/shared_config/
-
-# Make symlinks to bash files (for android do it manually)
-if [ -f $HOME/.bashrc ]; then
-    rm $HOME/.bashrc
-fi
-bashrc=$HOME/.bashrc
-if ! [ -L $bashrc ]; then
-    ln -s $BASE_DIR/bash/.bashrc $bashrc
-fi
-
-# Generate bash_completion file
-touch $HOME/.bash_completion
-echo "" > $HOME/.bash_completion
-for filename in $BASE_DIR/bash/bash_completion/*; do
-    echo "source ${BASE_DIR}/bash/bash_completion/${filename##*/}" >> $HOME/.bash_completion
-done
-
-# Make symlink for bin directory
-bindir=$HOME/.bin
-if ! [ -L $bindir ]; then
-    ln -s $BASE_DIR/bin $bindir
-fi
-
-# Make symlink for gitconfig
-gitconfig=$HOME/.gitconfig
-if ! [ -L $gitconfig ]; then
-    ln -s $BASE_DIR/git/.gitconfig $gitconfig
-fi
-
-# Make symlinks for vim
-vimconfig=$HOME/.vim
-if ! [ -L $vimconfig ]; then
-    ln -s $BASE_DIR/vim/.vim $vimconfig
-fi
-vimrc=$HOME/.vimrc
-if ! [ -L $vimrc ]; then
-    ln -s $BASE_DIR/vim/.vimrc $vimrc
-fi
 
 ###
 # Installs emacs configuration
@@ -81,7 +45,7 @@ configure_emacs() {
 ###
 install_essential_packages() {
     echo "Installing essential packages"
-    sudo emerge -av --autounmask y $(/bin/grep -v -R "^#" $BASE_DIR/essential_packages)
+    sudo emerge -av --autounmask y "$(/bin/grep -v -R "^#" $BASE_DIR/essential_packages)"
 }
 
 ###
@@ -119,7 +83,69 @@ install_powerline_prompt() {
     emerge -v powerline-go
 }
 
+###
+# Configure bash shell
+###
+configure_bash() {
+    # Make symlinks to common bash files
+    if [ -f $HOME/.bashrc ]; then
+        rm $HOME/.bashrc
+    fi
+    bashrc=$HOME/.bashrc
+    if ! [ -L $bashrc ]; then
+        ln -s $BASE_DIR/bash/.bashrc $bashrc
+    fi
+
+    # Generate bash_completion file
+    touch $HOME/.bash_completion
+    echo "" > $HOME/.bash_completion
+    for filename in $BASE_DIR/bash/bash_completion/*; do
+        echo "source ${BASE_DIR}/bash/bash_completion/${filename##*/}" >> $HOME/.bash_completion
+    done
+}
+
+###
+# Add custom binaries
+###
+function configure_custom_bin() {
+    # Make symlink for bin directory
+    bindir=$HOME/.bin
+    if ! [ -L $bindir ]; then
+        ln -s $BASE_DIR/bin $bindir
+    fi
+}
+
+###
+# Configure git
+###
+configure_git() {
+    gitconfig=$HOME/.gitconfig
+    if ! [ -L $gitconfig ]; then
+        ln -s $BASE_DIR/git/.gitconfig $gitconfig
+    fi
+}
+
+###
+# Install vim configuration
+###
+configure_vim() {
+    # Make symlinks for vim
+    vimconfig=$HOME/.vim
+    if ! [ -L $vimconfig ]; then
+        ln -s $BASE_DIR/vim/.vim $vimconfig
+    fi
+
+    vimrc=$HOME/.vimrc
+    if ! [ -L $vimrc ]; then
+        ln -s $BASE_DIR/vim/.vimrc $vimrc
+    fi
+}
+
 ask_install "Install essential packages" install_essential_packages
 ask_install "Add TapokOverlay repository" add_tapok_overlay
 ask_install "Install powerline shell prompt" install_powerline_prompt
 ask_install "Install emacs configuration" configure_emacs
+ask_install "Install vim configuration" configure_vim
+ask_install "Install git configuration" configure_git
+ask_install "Configure bash shell" configure_bash
+ask_install "Add custom binaries" configure_custom_bin
