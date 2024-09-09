df290c1491533d26d689101125c6e36eb71eb43e
Yahor Berdnikau <egorr.berd@gmail.com>
2024-09-09 10:03:00 +0200

    Update kbd_options.sh for wayland case

--- bin/kbd_options.sh
+++ bin/kbd_options.sh
@@ -5,16 +5,27 @@ set -euo pipefail
 # SUBSYSTEM=="input", ACTION=="add", ENV{ID_VENDOR_ID}=="04d9", ENV{ID_MODEL_ID}=="0192", RUN+="/bin/su - <user> -c '/path/to/kbd_options_udev.sh'"
 # SUBSYSTEM=="input", ACTION=="remove", ENV{ID_VENDOR_ID}=="04d9", ENV{ID_MODEL_ID}=="0192", RUN+="/bin/su - <user> -c '/path/to/kbd_options_udev.sh'"
 
+function configure_on_X11 {
+   DISPLAY=":0.0"
+   XAUTHORITY=$( find /tmp/xauth_* -type f )
+   export DISPLAY XAUTHORITY HOME
+
+   if xinput --list | grep -q "USB-HID Keyboard"; then
+       echo "Present"
+       setxkbmap -option -option grp:caps_toggle
+   else
+       echo "Not present"
+       setxkbmap -option -option ctrl:nocaps,grp:lctrl_toggle
+   fi
+}
+
+function configure_on_wayland {
+    "Run kbd_wayland.sh on user login"
+}
+
 sleep 1
-DISPLAY=":0.0"
-XAUTHORITY=$( find /tmp/xauth_* -type f )
-export DISPLAY XAUTHORITY HOME
-
-
-if xinput --list | grep -q "USB-HID Keyboard"; then
-    echo "Present"
-    setxkbmap -option -option grp:caps_toggle
+if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
+    configure_on_wayland
 else
-    echo "Not present"
-    setxkbmap -option -option ctrl:nocaps,grp:lctrl_toggle
+    configure_on_X11
 fi
