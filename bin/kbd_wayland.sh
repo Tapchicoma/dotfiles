1618a0e0b8c4fca2f8a5a512e3495cc00e587bad
Yahor Berdnikau <egorr.berd@gmail.com>
2025-01-01 12:13:37 +0100

    Add '--watch' option to xremap for wayland

--- bin/kbd_wayland.sh
+++ bin/kbd_wayland.sh
@@ -12,4 +12,4 @@ if ! test -f "$xremap_config"; then
     echo "Xremap config file is not available in $xremap_config, check that it exists."
 fi
 
-$xremap_bin $xremap_config 2>&1 &
+$xremap_bin $xremap_config --watch 2>&1 &
